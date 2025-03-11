25450ee71207cb0454b1a6d7a605ca8c4a298acd
MatchaKim <kimjunsik333@gmail.com>
2025-03-11 11:09:06 +0900

    chore : eslint 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 import path from "path";
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
   webpack: (config) => {
     config.module.rules.push({
